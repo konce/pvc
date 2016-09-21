c8e48f80a53609976a2f889b4360c63b76450a45
jarvanxing <jarvan.xing@dji.com>
2016-09-21 18:12:35 +0800

    version

--- tools/build.js
+++ tools/build.js
@@ -138,6 +138,7 @@ function doFinal() {
   delete pkg.babel
   fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8')
   fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8')
+  fs.writeFileSync('dist/README.md', fs.readFileSync('README.md', 'utf-8'), 'utf-8')
   shell.exec('cp -rf bin dist')
 }
 
